213dbe981e4bc7c9fa82fecb30019f9907f91b2b
Bu Kinoshita <bukinoshita@gmail.com>
2018-12-28 21:53:05 -0200

    patch: fix color theme to use from ui theme (#16)

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ import Document, { Head, Main, NextScript } from 'next/document'
 // Root
 import pkg from '../package'
 
+// UI
+import { colors } from '../ui/theme'
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -18,7 +21,7 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="static/manifest.json" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <meta name="theme-color" content="#0e1012" />
+          <meta name="theme-color" content={colors.black} />
           <meta name="description" content={pkg.description} />
           <meta name="keywords" content={pkg.keywords} />
         </Head>
